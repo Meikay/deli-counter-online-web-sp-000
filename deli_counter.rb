07a61b7d620b674af0233cddb9a359b7d96260a6
Meisuleen Kased <meisuleen@gmail.com>
2018-11-10 19:37:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(other_deli)
-  if katz_deli.empty?
+  if other_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     #serve katz_deli.length
